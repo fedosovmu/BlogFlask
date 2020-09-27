11bfbb06ac92cd5c6a29610f705cea3bcbf30440
fedosovmu <fedosovmu@gmail.com>
2020-09-27 17:05:23 +0300

    Красивый вывод даты

--- Post.py
+++ Post.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+
 class Post:
-    def __init__(self, post_id, title, short_description, publication_date, img_url):
+    def __init__(self, post_id, title, short_description, publication_date_iso_format, img_url):
         self.post_id = post_id
         self.title = title
         self.short_description = short_description
-        self.publication_date = publication_date
+        self.publication_date = datetime.fromisoformat(publication_date_iso_format)
         self.img_url = img_url
+
+    def get_publication_date_str(self):
+        return self.publication_date.strftime("%m/%d/%Y, %H:%M:%S")
